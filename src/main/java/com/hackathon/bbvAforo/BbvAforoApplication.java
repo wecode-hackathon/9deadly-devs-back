c952bc2e7059f4214c561cce9ee52d50c816bf9f
Deiroo <renatoballon.rb@gmail.com>
2022-08-27 11:48:00 -0500

    first back commit

---
+++ src/main/java/com/hackathon/bbvAforo/BbvAforoApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.bbvAforo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbvAforoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BbvAforoApplication.class, args);
+	}
+
+}
