26cb19bb98adbdf57b166eee6c5b0202d894e94e
Deiroo <renatoballon.rb@gmail.com>
2022-08-27 15:54:37 -0500

    getOffices endpoint

--- src/main/java/com/hackathon/bbvAforo/controller/CapacidadActualController.java
+++ src/main/java/com/hackathon/bbvAforo/controller/CapacidadActualController.java
@@ -3,20 +3,25 @@ package com.hackathon.bbvAforo.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.hackathon.bbvAforo.dto.Oficina;
+
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.websocket.server.PathParam;
 
 @RestController
 public class CapacidadActualController {
-    
+
     @GetMapping("/obtenerCapacidadActual")
     public int greeting(@PathParam(value = "idOficina") int idOficina) {
         return 1;
     }
 
     @GetMapping("/obtenerOficinas")
-    public int getOficinas(@PathParam(value = "latUsuario") BigDecimal latUsuario, @PathParam(value = "longUsuario") BigDecimal longUsuario) {
-        return 1;
+    public List<Oficina> getOficinas(@PathParam(value = "latUsuario") BigDecimal latUsuario, @PathParam(value = "longUsuario") BigDecimal longUsuario) {
+        ArrayList<Oficina> oficinas = new ArrayList<Oficina>();
+        return oficinas;
     }
 }
