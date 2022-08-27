48706d19c8140a65487fcd8e6d9a0f7b5c465008
Deiroo <renatoballon.rb@gmail.com>
2022-08-27 17:34:33 -0500

    adding queries to get offices

--- src/main/java/com/hackathon/bbvAforo/controller/CapacidadActualController.java
+++ src/main/java/com/hackathon/bbvAforo/controller/CapacidadActualController.java
@@ -7,7 +7,7 @@ import com.hackathon.bbvAforo.dto.Oficina;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
-import java.util.List;
+import java.sql.*;
 
 import javax.websocket.server.PathParam;
 
@@ -20,8 +20,35 @@ public class CapacidadActualController {
     }
 
     @GetMapping("/obtenerOficinas")
-    public List<Oficina> getOficinas(@PathParam(value = "latUsuario") BigDecimal latUsuario, @PathParam(value = "longUsuario") BigDecimal longUsuario) {
+    public ArrayList<Oficina> getOficinas(@PathParam(value = "latUsuario") BigDecimal latUsuario, @PathParam(value = "longUsuario") BigDecimal longUsuario) throws SQLException {
         ArrayList<Oficina> oficinas = new ArrayList<Oficina>();
+
+        String url = "192.168.160.148:3306";
+        Connection conn = DriverManager.getConnection(url,"grupo4","clark");
+
+
+        BigDecimal latProcesada1 = latUsuario;
+        BigDecimal latProcesada2 = latUsuario;
+
+        BigDecimal longProcesada1 = longUsuario;
+        BigDecimal longProcesada2 = longUsuario;
+
+        BigDecimal diff = new BigDecimal(0.01);
+
+        latProcesada1 = latProcesada1.subtract(diff);
+        latProcesada2 = latProcesada2.subtract(diff);
+
+        longProcesada1 = longProcesada1.subtract(diff);
+        longProcesada2 = longProcesada2.subtract(diff);
+        
+        Statement stmt = conn.createStatement();
+        String query = "SELECT * FROM oficinas where latitud between("+ latProcesada1 +") and ("+ latProcesada2 +") and longitud between ("+ longProcesada1 +") and ("+ longProcesada2 +");";
+        
+
+        ResultSet rs = stmt.executeQuery(query);
+
+        
+
         return oficinas;
     }
 }
