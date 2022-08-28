685ac6a90ab08dd9a8c1416c679df6f1320f9b4b
Deiroo <renatoballon.rb@gmail.com>
2022-08-27 23:13:35 -0500

    adding second query

--- src/main/java/com/hackathon/bbvAforo/repository/CapacidadActualRepository.java
+++ src/main/java/com/hackathon/bbvAforo/repository/CapacidadActualRepository.java
@@ -38,4 +38,34 @@ public class CapacidadActualRepository {
                     return oficina;
                 });
     }
+
+    public List<Oficina> getOficinas(BigDecimal latitud, BigDecimal longitud) {
+        BigDecimal latProcesada1 = latitud;
+        BigDecimal latProcesada2 = latitud;
+    
+        BigDecimal longProcesada1 = longitud;
+        BigDecimal longProcesada2 = longitud;
+    
+        BigDecimal diff = new BigDecimal(0.01);
+    
+        latProcesada1 = latProcesada1.subtract(diff);
+        latProcesada2 = latProcesada2.subtract(diff);
+    
+        longProcesada1 = longProcesada1.subtract(diff);
+        longProcesada2 = longProcesada2.subtract(diff);
+
+        return this.jdbcTemplate.query(
+                "SELECT * FROM oficinas where latitud between(?) and (?) and longitud between (?) and (?);",
+                new Object[] { latProcesada1, latProcesada2, longProcesada1, longProcesada2 },
+                (rs, rowNum) -> {
+                    Oficina oficina = new Oficina();
+                    oficina.setAforoActual(rs.getInt("aforoactual"));
+                    oficina.setNombre(rs.getString("nombre"));
+                    oficina.setDireccion(rs.getString("direccion"));
+                    oficina.setLatOficina(rs.getBigDecimal("latitud"));
+                    oficina.setLongOficina(rs.getBigDecimal("longitud"));
+
+                    return oficina;
+                });
+    }
 }
---
+++ src/main/java/com/hackathon/bbvAforo/service/CapacidadActualService.java
@@ -0,0 +1,26 @@
+package com.hackathon.bbvAforo.service;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.hackathon.bbvAforo.dto.Oficina;
+import com.hackathon.bbvAforo.repository.CapacidadActualRepository;
+
+@Service
+public class CapacidadActualService {
+
+    private CapacidadActualRepository capacidadActualRepository;
+
+    public CapacidadActualService(CapacidadActualRepository capacidadActualRepository ){
+        this.capacidadActualRepository = capacidadActualRepository;
+    }
+
+    public List<Oficina> getOficinas(BigDecimal latitud, BigDecimal longitud) {
+        return capacidadActualRepository.getOficinas(latitud, longitud);
+        
+    }
+
+    
+}
